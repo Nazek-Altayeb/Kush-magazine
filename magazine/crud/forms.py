47d972b7eec02a0a1ebed7fc8a4ab2041251cfd3
Nazek-Altayeb <>
2023-05-29 15:51:35 +0000

    extend add_article method

--- magazine/crud/forms.py
+++ magazine/crud/forms.py
@@ -1,13 +1,14 @@
 from django import forms
-from .models import Topic, Article
+from .models import Article
 
 
 class ArticleForm(forms.ModelForm):
-    title = forms.CharField()
-    body = forms.CharField()
-    topics = forms.ModelChoiceField(
-        queryset=Topic.objects.values_list('topic', flat=True).distinct())
-
     class Meta:
         model = Article
-        fields = ('title', 'body', 'topic',)
+        fields = '__all__'
+
+
+"""   title = forms.CharField()
+    body = forms.CharField()
+    topics = forms.ModelChoiceField(
+        queryset=Topic.objects.values_list('topic', flat=True).distinct())"""
--- magazine/crud/views.py
+++ magazine/crud/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Article, Topic
+from .forms import ArticleForm
 # from .forms import DropDown
 
 # Create your views here.
 
 
-def get_article(request):
+"""def get_article(request):
     articles = Article.objects.all()
-    topics = Topic.objects.all()
     context = {
         'articles': articles
     }
-    return render(request, 'crud/get_article.html', context)
+    return render(request, 'crud/get_article.html', context)"""
 
 
 def add_article(request):
     if request.method == "POST":
         article_title = request.POST.get('article_title')
         article_body = request.POST.get('article_body')
-
-        Article.objects.create(title=article_title, body=article_body)
+        article_topic = request.POST.get('article_topic')
+        Article.objects.create(title=article_title,
+                               body=article_body, topic=article_topic)
         return redirect('get_article')
     return render(request, 'crud/add_article.html')
 
@@ -39,3 +40,15 @@ def add_topic(request):
         Topic.objects.create(name=topic_name)
         return redirect('get_topic')
     return render(request, 'topics/add_topic.html')
+
+
+def get_article(request):
+    form = ArticleForm()
+    # topics = Topic.objects.all()
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+       # form = ArticleForm(request.POST, instance=Article.objects.first())
+        if form.is_valid():
+            form.save()
+            return redirect('get_article')
+    return render(request, 'crud/add_article.html', {'form': form})
